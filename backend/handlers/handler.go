1eb7a045e32ca30278ee43865a27bbfac1856ef3
allkamau <allangithaiga5@gmail.com>
2025-03-05 12:29:40 +0300

    Add HomeHandler for serving the homepage and redirecting authenticated users

---
+++ backend/handlers/handler.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Githaiga22/forum/backend/repositories"
+	"github.com/Githaiga22/forum/backend/util"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		util.ErrorHandler(w, "Page does not exist", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		log.Println("Method not allowed", r.Method)
+		util.ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	cookie, _ := getSessionID(r)
+	_, ok := SessionStore[cookie]
+	if ok {
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
+		return
+	}
+
+	// Load posts
+	posts, err := repositories.GetPosts(util.Database)
+	if err != nil {
+		log.Printf("Failed to get posts: %v", err)
+		util.ErrorHandler(w, "An Unexpected Error Occurred. Try Again Later", http.StatusInternalServerError)
+		return
+	}
+
+	PostDetails(w, r, posts, false)
+}
