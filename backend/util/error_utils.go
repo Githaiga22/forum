5da8965963521899f881d3b4c4cf5c0cb8b764e1
allkamau <allangithaiga5@gmail.com>
2025-03-05 14:34:19 +0300

    Add ErrorHandler utility for handling and rendering error responses

---
+++ backend/util/error_utils.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+type Message struct {
+	Code       string
+	ErrMessage string
+}
+
+func ErrorHandler(w http.ResponseWriter, errval string, statusCode int) {
+	tmpl, err := template.ParseFiles("frontend/templates/error.html")
+	if err != nil {
+		log.Printf("Failed to load error template: %v", err)
+		http.Error(w, errval, statusCode)
+		return
+	}
+
+	code := strconv.Itoa(statusCode)
+
+	data := Message{
+		Code:       code,
+		ErrMessage: errval,
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("Error executing the template: %v", err)
+		http.Error(w, "An Unexpected Error Occurred. Try Again Later", http.StatusInternalServerError)
+	}
+}
