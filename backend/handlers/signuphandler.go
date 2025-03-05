4cd43ad6c406a78b32bcb7cfc18546137532be7b
allkamau <allangithaiga5@gmail.com>
2025-03-05 12:33:01 +0300

    Add SignupHandler for user registration with form validation and password encryption

---
+++ backend/handlers/signuphandler.go
@@ -0,0 +1,84 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
+	"text/template"
+
+	"github.com/Githaiga22/forum/backend/models"
+	"github.com/Githaiga22/forum/backend/repositories"
+	"github.com/Githaiga22/forum/backend/util"
+)
+
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	if r.URL.Path != "/sign-up" {
+		util.ErrorHandler(w, "Page does not exist", http.StatusNotFound)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			log.Printf("Failed parsing form: %v\n", err)
+			util.ErrorHandler(w, "An Unexpected Error Occurred. Try Again Later", http.StatusInternalServerError)
+			return
+		}
+
+		user.Username = strings.TrimSpace(r.FormValue("username"))
+		user.Email = strings.TrimSpace(r.FormValue("email"))
+		user.Password = strings.TrimSpace(r.FormValue("password"))
+		user.ConfirmedPassword = strings.TrimSpace(r.FormValue("confirmed-password"))
+
+		err = util.ValidateFormFields(user.Username, user.Email, user.Password)
+		if err != nil {
+			log.Printf("Invalid form values from user: %v\n", err)
+			response := Response{Success: false}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		if !reflect.DeepEqual(user.Password, user.ConfirmedPassword) {
+			log.Printf("User password %q and confirmed password %q do not match.\n", user.Password, user.ConfirmedPassword)
+			response := Response{Success: false}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+
+		hashed, err := util.PasswordEncrypt([]byte(user.Password), 10)
+		if err != nil {
+			log.Printf("Failed encrypting password: %v\n", err)
+			util.ErrorHandler(w, "An Unexpected Error Occurred. Try Again Later", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = repositories.InsertRecord(util.Database, "tblUsers", []string{"username", "email", "user_password"}, user.Username, user.Email, string(hashed))
+		if err != nil {
+			log.Println("Error adding user:", err)
+			http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+			return
+		}
+		response := Response{Success: true}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+		http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+		return
+	} else if r.Method == http.MethodGet {
+		tmpl, err := template.ParseFiles("frontend/templates/sign-up.html")
+		if err != nil {
+			log.Println("failed parsing files:", err)
+			util.ErrorHandler(w, "An Unexpected Error Occurred. Try Again Later", http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
+	} else {
+		log.Println("Method not allowed", r.Method)
+		util.ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
