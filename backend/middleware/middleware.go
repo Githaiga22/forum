e360ec3dd6bb6a0f054dfc5b6153691d032ce18c
allkamau <allangithaiga5@gmail.com>
2025-03-05 14:26:48 +0300

    Add Authenticate middleware for session token validation and user context management

---
+++ backend/middleware/middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/Githaiga22/forum/backend/repositories"
+)
+
+type contextSession string
+
+var newSession contextSession = "userId"
+
+// Authenticate middleware to check session token
+func Authenticate(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("session_token")
+		if err != nil {
+			log.Println("NO session token", err)
+			http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+			return
+		}
+
+		userID, err := repositories.ValidateSession(cookie.Value)
+		if err != nil {
+			log.Printf("Invalid session token: %v", err)
+			http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), newSession, userID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
